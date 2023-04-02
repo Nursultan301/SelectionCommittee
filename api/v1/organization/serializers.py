800502c84be674e2bf886c395f9151ba70179b97
Nursultan301 <abykeevnursultan14@gmail.com>
2023-04-02 14:36:18 +0600

    refac: Change serializers

--- api/v1/organization/serializers.py
+++ api/v1/organization/serializers.py
@@ -1,12 +1,27 @@
 from rest_framework import serializers
 
-from apps.organization.models import Entrant
+from apps.organization.models import Entrant, Specialty
 
 
 class EntrantSerializer(serializers.ModelSerializer):
     """ Список абитуриентов """
-    classing = serializers.CharField(source='get_classing_display')
+    area = serializers.CharField(source='get_area_display')
+    base_class = serializers.CharField(source='get_base_class_display')
+    nationality = serializers.CharField(source='get_nationality_display')
+    gender = serializers.CharField(source='get_gender_display')
+    form_training = serializers.CharField(source='get_form_training_display')
+    basic_training = serializers.CharField(source='get_basic_training_display')
+    status = serializers.CharField(source='get_status_display')
 
     class Meta:
         model = Entrant
-        exclude = ('active',)
+        fields = "__all__"
+
+
+class SpecialtySerializer(serializers.ModelSerializer):
+    form_training = serializers.CharField(source='get_form_training_display')
+    basic_training = serializers.CharField(source='get_basic_training_display')
+
+    class Meta:
+        model = Specialty
+        fields = "__all__"
