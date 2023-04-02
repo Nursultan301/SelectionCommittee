bfccf0f8e15cc34be73da0382967c781a5993e47
Nursultan301 <abykeevnursultan14@gmail.com>
2023-04-02 14:35:24 +0600

    feat: Adding new constants

--- common/constants.py
+++ common/constants.py
@@ -1,4 +1,45 @@
+#  GENDER_STATUS
 MEN: str = 'men'
 WOMEN: str = 'women'
+
+#  NATIONALITY
+KYRGYZ: str = 'kyrgyz'
+RUSSIAN: str = 'russian'
+KAZAKH: str = 'kazakh'
+UZBEK: str = 'uzbek'
+TAJIK: str = 'tajik'
+OTHER: str = 'other'
+
+#  AREA
+BATKEN: str = 'batken'
+DJALAL_ABAD: str = 'djalal_abad'
+ISSYK_KUL: str = 'issyk_kul'
+NARYN: str = 'naryn'
+OSH: str = 'osh'
+TALAS: str = 'talas'
+CHYI: str = 'chyi'
+BISHKEK: str = 'bishkek'
+
+#  GENDER_STATUS
+FULL_TIME: str = 'full_time'
+PART_TIME: str = 'part_time'
+
+#  BASIC_STATUS
+BUDGET: str = 'budget'
+CONTRACT: str = 'contract'
+
+#  STATUS
+RENDING_REVIEW: str = 'rending_review'
+APPROVE: str = 'approve'
+REJECT: str = 'reject'
+
+#  CLASSING
 CLASSING_9: str = 'classing_9'
 CLASSING_11: str = 'classing_11'
+
+#  FORM_STATUS
+FULL_PART_TIME: str = 'full_part_time'
+
+#  BASIC_STATUS
+BUDGET_CONTRACT: str = 'budget_contract'
+
